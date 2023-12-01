075e0955f0db44f9abe7b307a9358182ff0affc7
irebm2 <irebazaga@gmail.com>
2023-12-01 22:12:53 +0000

    ok - logic and questions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,50 +1,104 @@
 // Select the start button element from the document.
 const startButton = document.querySelector('#start');
-
 // Select the timer display element from the document.
 const timerDisplay = document.querySelector('#time');
-
 // Initialize the timeValue to 75 seconds.
-var timeValue = 75;
-
+let timeValue = 75;
 // Select the quiz start screen element from the document.
 const StartQuizChallenge = document.querySelector('#start-screen');
-
 // Create element for the question div
 const Question = document.querySelector('#questions');
-
+Question.style.display = 'none'; // Hide the questions initially
 // Create element for the question title element
 const QuestionTitle = document.querySelector('#question-title');
-
 // Create element for the choice div
 const Choice = document.querySelector('#choices');
+// Create a variable to store the question index
+let questionIndex = 0;
 
 // Add an event listener to the start button that triggers when clicked.
 startButton.addEventListener('click', function (e) {
-    // Update the text of timer display to show the timeValue in seconds.
-    timerDisplay.textContent = timeValue + ' seconds';
-
-    // Set up a recurring interval function that runs every 1000ms (1 second).
-    var countdownInterval = setInterval(timePass, 1000);
-
-    // Define the timePass function.
-    function timePass() {
-        // Decrease timeValue by one.
-        --timeValue;
-
-        // Update timer display with the new time value in seconds.
-        timerDisplay.textContent = timeValue + ' seconds';
-
-        // Check if timeValue has reached or goes below zero.
-        if (timeValue <= 0) {
-            // If yes, then clear the interval function, thus stopping the countdown.
-            clearInterval(countdownInterval);
-
-            // Update timer display to say 'Time is up!'
-            timerDisplay.textContent = 'Time is up!';
-        }
-    }
+  startQuiz();
 });
 
-// Add an event listener to the start button that triggers the StartQuiz function when clicked.
-startButton.addEventListener('click', StartQuiz);
+function startQuiz() {
+  // Update the text of timer display to show the timeValue in seconds.
+  timerDisplay.textContent = timeValue + ' seconds';
+  // Set up a recurring interval function that runs every 1000ms (1 second).
+  const countdownInterval = setInterval(timePass, 1000);
+
+  // Define the timePass function.
+  function timePass() {
+    // Decrease timeValue by one.
+    timeValue--;
+    // Update timer display with the new time value in seconds.
+    timerDisplay.textContent = timeValue + ' seconds';
+    // Check if timeValue has reached or goes below zero.
+    if (timeValue <= 0) {
+      // If yes, then clear the interval function, thus stopping the countdown.
+      clearInterval(countdownInterval);
+      // Update timer display to say 'Time is up!'
+      timerDisplay.textContent = 'Time is up!';
+      finishQuiz();
+    }
+  }
+
+  // Hide the start screen element.
+  StartQuizChallenge.style.display = 'none';
+  // Show the questions element.
+  Question.style.display = 'block';
+  // Display the current question
+  displayQuestion(questionIndex);
+}
+
+function displayQuestion(index) {
+  QuestionTitle.textContent = quizData[index].question;
+  const choices = document.getElementById('choices');
+  choices.innerHTML = '';
+
+  // Create the answer buttons dynamically
+  for (let i = 0; i < quizData[index].answers.length; i++) {
+    const answer = document.createElement('button');
+    answer.textContent = quizData[index].answers[i];
+    choices.appendChild(answer);
+    answer.addEventListener('click', function () {
+      checkAnswer(answer);
+    });
+  }
+}
+
+function checkAnswer(selectedAnswer) {
+  const answerText = selectedAnswer.textContent;
+  const feedbackDiv = document.getElementById('feedback');
+  feedbackDiv.style.display = 'block';
+  const correctAnswer = quizData[questionIndex].correctAnswer;
+
+  if (answerText === correctAnswer) {
+    feedbackDiv.textContent = 'Right answer!';
+  } else {
+    feedbackDiv.textContent = 'Wrong answer!';
+    timeValue -= 10;
+  }
+
+  questionIndex++;
+  if (questionIndex < quizData.length) {
+    displayQuestion(questionIndex);
+  } else {
+    finishQuiz();
+  }
+}
+
+function finishQuiz() {
+  const score = calculateScore();
+  // Add your own logic here to handle the end of the quiz, such as displaying the score or redirecting the user to another page.
+  console.log('Quiz finished');
+  console.log('Score: ' + score);
+}
+
+function calculateScore() {
+  const correctAnswersCount = quizData.filter(
+    (question) => question.correctAnswer === answerText
+  ).length;
+
+  return correctAnswersCount * timeValue;
+}
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,88 +1,42 @@
-// Define the StartQuiz function.
-function StartQuiz() {
-    // Immediately hide the quiz start screen element.
-    StartQuizChallenge.style.display = 'none';
-
-    // Show the questions element.
-    Question.style.display = 'block';
-
-    // Show question 1 in the start screen and Choices
-    QuestionTitle.textContent = 'JavaScript File Has An Extension of:';
-
-    // Create the answer1 button element.
-    const answer1 = document.createElement('button');
-    answer1.textContent = '.java';
-
-    // Append the answer1 button to the Choice element.
-    Choice.appendChild(answer1);
-
-    // Repeat for the other choice elements
-    const answer2 = document.createElement('button');
-    answer2.textContent = '.java-script';
-    Choice.appendChild(answer2);
-
-    const answer3 = document.createElement('button');
-    answer3.textContent = '.js';
-    Choice.appendChild(answer3);
-
-    const answer4 = document.createElement('button');
-    answer4.textContent = '.xml';
-    Choice.appendChild(answer4);
-
-    // Add an EventListener to each answer element
-    answer1.addEventListener('click', checkAnswer);
-    answer2.addEventListener('click', checkAnswer);
-    answer3.addEventListener('click', checkAnswer);
-    answer4.addEventListener('click', checkAnswer);
-}
-
-function checkAnswer(event) {
-    const answerText = event.target.textContent;
-
-    const feedbackDiv = document.querySelector('#feedback');
-    feedbackDiv.style.display = 'block';
-
-    // In this case, the correct answer is '.js'.
-    if (answerText === '.js') {
-        feedbackDiv.textContent = 'Right answer!';
-    } else {
-        feedbackDiv.textContent = 'Wrong answer!';
-        // if the answer is not correct then display -10 seconds in the timer
-        timeValue -= 10;
-    }
-
-    // Remove the event listeners from the first set of answer buttons
-    answer1.removeEventListener('click', checkAnswer);
-    answer2.removeEventListener('click', checkAnswer);
-    answer3.removeEventListener('click', checkAnswer);
-    answer4.removeEventListener('click', checkAnswer);
-
-    // Update the second question and answer options
-    QuestionTitle.textContent = 'The Tag is used To Give Heading To The Table.';
-    answer1.textContent = 'Caption';
-    answer2.textContent = 'Head';
-    answer3.textContent = 'Td';
-    answer4.textContent = 'Th';
-
-    // Add new event listeners to the second set of answer buttons
-    answer1.addEventListener('click', checkAnswer_2);
-    answer2.addEventListener('click', checkAnswer_2);
-    answer3.addEventListener('click', checkAnswer_2);
-    answer4.addEventListener('click', checkAnswer_2);
-}
-
-function checkAnswer_2(event) {
-    const answerText = event.target.textContent;
-
-    const feedbackDiv = document.querySelector('#feedback');
-    feedbackDiv.style.display = 'block';
-
-    // In this case, the correct answer is 'Caption'.
-    if (answerText === 'Caption') {
-        feedbackDiv.textContent = 'Right answer!';
-    } else {
-        feedbackDiv.textContent = 'Wrong answer!';
-        // if the answer is not correct then display -10 seconds in the timer
-        timeValue -= 10;
-    }
-}
+const quizData = [
+    {
+      question: 'JavaScript File Has An Extension of:',
+      answers: ['.java', '.java-script', '.js', '.xml'],
+      correctAnswer: '.js',
+    },
+    {
+      question: 'The Tag is used To Give Heading To The Table.',
+      answers: ['Caption', 'Head', 'Td', 'Th'],
+      correctAnswer: 'Caption',
+    },
+    {
+      question: 'IsNaN() Evaluates And Argument To Determine if Given Value:',
+      answers: [
+        'Is Not a Null',
+        'Is Not a Number',
+        'Is Not a New Object',
+        'None of the above',
+      ],
+      correctAnswer: 'Is Not a Number',
+    },
+    {
+      question: 'Which of the following is NOT a JavaScript data type?',
+      answers: ['String', 'Boolean', 'Function', 'Integer'],
+      correctAnswer: 'Integer',
+    },
+    {
+      question: 'What is the result of the statement "3" + 2 in JavaScript?',
+      answers: ['32', '5', 'NaN', 'TypeError'],
+      correctAnswer: '32',
+    },
+    {
+      question: 'What does the "this" keyword refer to in JavaScript?',
+      answers: [
+        'The current element',
+        'The parent element',
+        'The global object',
+        'The previous sibling',
+      ],
+      correctAnswer: 'The current element',
+    },
+  ];
