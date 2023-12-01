32a02ba2e9f5f53bbfc7ea4a00e3bb01dde1e197
irebm2 <irebazaga@gmail.com>
2023-12-01 20:55:06 +0000

    ok- add answers

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -10,13 +10,19 @@ var timeValue = 75;
 // Select the quiz start screen element from the document.
 const StartQuizChallenge = document.querySelector('#start-screen');
 
-// Select the question
+// Create element for the question div
 const Question = document.querySelector('#questions');
+
+// Create element for the Question title element
+const QuestionTitle = document.querySelector('#question-title');
+
+// Create element for the choice div
+const Choice = document.querySelector('#choice');
 
 // Add an event listener to the start button that triggers when clicked.
 startButton.addEventListener('click', function (e) {
 
-    // Update the text of timer display element to show the timeValue in seconds.
+    // Update the text of timer display to show the timeValue in seconds.
     timerDisplay.textContent = timeValue + ' seconds';
 
     // Set up a recurring interval function that runs every 1000ms (1 second).
@@ -31,13 +37,13 @@ startButton.addEventListener('click', function (e) {
         // Update timer display with the new time value in seconds.
         timerDisplay.textContent = timeValue + ' seconds';
 
-        // Check if timeValue reaches or goes below zero.
+        // Check if timeValue has reached or goes below zero.
         if (timeValue <= 0) {
 
             // If yes, then clear the interval function, thus stopping the countdown.
             clearInterval(countdownInterval);
 
-            // Update timer display to say "Time is up!"
+            // Update timer display to say 'Time is up!'
             timerDisplay.textContent = 'Time is up!';
         }
     }
@@ -52,20 +58,16 @@ function StartQuiz () {
     // Immediately hide the quiz start screen element.
     StartQuizChallenge.style.display = 'none';
 
-    // Replace the class 'show' with 'hide' for StartQuizChallenge element.
-    StartQuizChallenge.classList.replace('show','hide');
-    // Replace the class 'hide' with 'show' for Question element.
-    Question.classList.replace('hide','show');
+    // Show the questions element.
+    Question.style.display = 'block';
 
-    // Check if the classList of StartQuizChallenge contains 'hide'.
-    if (StartQuizChallenge.classList.contains('hide')) {
+    // Show question 1 in the start screen and Choices
+    QuestionTitle.textContent = 'JavaScript File Has An Extension of:';
 
-        // If yes, then hide the StartQuizChallenge element.
-        StartQuizChallenge.style.display = 'none';    
-    }
-    if (Question.classList.contains('hide')) {
+    // Create the answer1 button element.
+    const answer1 = document.createElement('button');
+    answer1.textContent  = '.java';
 
-        // If yes, then hide the StartQuizChallenge element.
-        Question.style.display = 'none';    
-    } else {Question.style.display = 'block';}
+    // Append the answer1 button to the Choice element.
+    Choice.appendChild(answer1);
 }
